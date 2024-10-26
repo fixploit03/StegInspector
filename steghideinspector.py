e129acd0165e05d939953947e4f581b48e44c6fe
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-26 10:00:29 +0700

    Create steghideinspector.py

---
+++ steghideinspector.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+
+file_stego = input("[#] Masukkan nama file stego: ")
+
+print(f"[*] Mengecek file stego '{file_stego}'...")
+time.sleep(3)
+
+perintah_cek_file_stego = f"strings {file_stego}"
+
+try:
+    cek_file_stego = subprocess.run(perintah_cek_file_stego, shell=True, capture_output=True, text=True)
+    if cek_file_stego.returncode == 0:
+        pola_file_steghide = r"%&'\(\)\*456789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz\n\s*#3R\n&'\(\)\*56789:CDEFGHIJSTUVWXYZcdefghijstuvwxyz"
+        if re.search(pola_file_steghide, cek_file_stego.stdout):
+            print(f"[+] File '{file_stego}' adalah file stego.")
+        else:
+            print(f"[-] File '{file_stego}' bukan file stego.{r}")
+except Exception as e:
+  print(f"[-] Terjadi kesalahan: {e}")
